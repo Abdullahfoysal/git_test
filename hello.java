f4a6c19cd4a6c00e858df8a5a6e3a00cc04d17c2
Abdullahfoysal <abdullah32@student.sust.edu>
2019-02-14 02:17:23 +0600

    print hello java

--- hello.java
+++ hello.java
@@ -1,6 +1,15 @@
 public class hello{
 
+
 	public static void main(String[] args){
 		System.out.println("sublime is also awesome");
+	    greeting("Hello java");
 	}
+
+	public static void greeting(String name)
+	{
+		System.out.println(name);
+	}
+
+	
 }
