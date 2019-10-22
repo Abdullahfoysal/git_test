2263436afe6a2a38ffd713e91134c13369adee1b
Abdullahfoysal <abdullah32@student.sust.edu>
2019-10-22 20:31:11 +0600

    check marge

---
+++ hello.java
@@ -0,0 +1,22 @@
+public class hello{
+	public	hello(){
+
+
+		int fooysal;
+
+	}
+
+	public static void main(String[] args){
+		System.out.println("sublime is also awesome");
+	    greeting("Hello java");
+	}
+
+	public static void greeting(String name)
+	{
+		System.out.println(name);
+	}
+	public static int sqrt(int n)
+	{
+		return n*n;
+	}
+}
